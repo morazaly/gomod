fd5caf24b85ec782ea361c95197cf4014fb24969
omashay <orlik19or@mail.ru>
2022-12-30 18:29:24 +0600

    second c

--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func main() {
-	//var ntptime , err string
-	err := "ss"
-	//ntptime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
-	if err == "nil" {
-		fmt.Println("ss")
-		os.Exit(1)
-		//fmt.Println(os.Stderr, ntptime)
+func strup(s string) string {
+	var res string
+	prev := ""
+	for pos, char := range s {
+		intd, err := strconv.Atoi(string(char))
+		if _, errr := strconv.Atoi(string(prev)); errr == nil && err == nil {
+			return "not valid"
+		}
+		if err == nil {
 
-	} else {
+			for i := 1; i < intd; i++ {
+				res = res + string(prev)
+			}
+
+		} else {
+			res = res + string(char)
+
+		}
+		if pos > pos-1 {
+			prev = string(char)
+		}
+
+	}
+
+	return res
+
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	text = strup(text)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
+
+	} else {
+		fmt.Println(text)
+		os.Exit(1)
 	}
 }
